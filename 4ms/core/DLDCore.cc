e79bbc704af7a2f92bd0097017311fad40551331
Dan Green <danngreen1@gmail.com>
2024-09-16 10:08:44 -0700

    DLD: format

--- 4ms/core/DLDCore.cc
+++ 4ms/core/DLDCore.cc
@@ -1,9 +1,9 @@
 #include "CoreModules/4ms/info/DLD_info.hh"
 //
-#include "patch-serial/base64.hh"
 #include "CoreModules/SmartCoreProcessor.hh"
 #include "CoreModules/moduleFactory.hh"
 #include "looping-delay/DLDChannel.hh"
+#include "patch-serial/base64.hh"
 #include <alpaca/alpaca.h>
 
 namespace MetaModule
@@ -16,88 +16,82 @@ public:
 	using enum Info::Elem;
 
 	template<Info::Elem EL>
-	void setOutput(auto val)
-	{
+	void setOutput(auto val) {
 		return SmartCoreProcessor<Info>::setOutput<EL>(val);
 	}
 
 	template<Info::Elem EL>
-	auto getInput()
-	{
+	auto getInput() {
 		return SmartCoreProcessor<Info>::getInput<EL>();
 	}
 
 	template<Info::Elem EL, typename VAL>
-	void setLED(const VAL &value)
-	{
+	void setLED(const VAL &value) {
 		return SmartCoreProcessor<Info>::setLED<EL>(value);
 	}
 
 	template<Info::Elem EL>
-	auto getState()
-	{
+	auto getState() {
 		return SmartCoreProcessor<Info>::getState<EL>();
 	}
 
 private:
-	struct MappingA
-	{
-		const static Info::Elem TimeKnob              = TimeAKnob;
-		const static Info::Elem FeedbackKnob          = FeedbackAKnob;
-		const static Info::Elem DelayFeedKnob         = DelayFeedAKnob;
-		const static Info::Elem MixKnob               = MixAKnob;
-		const static Info::Elem ReverseButton         = ReverseAButton;
-		const static Info::Elem HoldButton            = HoldAButton;
-		const static Info::Elem ReturnInput           = ReturnAIn;
-		const static Info::Elem SendOutput            = SendAOut;
-		const static Info::Elem ReverseInput          = ReverseAJackIn;
-		const static Info::Elem HoldInput             = HoldAJackIn;
-		const static Info::Elem TimeInput             = TimeAJackIn;
-		const static Info::Elem FeedbackInput         = FeedbackAJackIn;
-		const static Info::Elem DelayFeedInput        = DelayFeedAJackIn;
-		const static Info::Elem LoopOutput            = LoopAOut;
-		const static Info::Elem TimeLight             = TimeALedLight;
-		const static Info::Elem TimeModSwitch         = TimeMultASwitch;
-		const static Info::Elem SoftClipEnabledAlt    = SoftClipAAltParam;
-		const static Info::Elem AutoMuteEnabledAlt    = AutomuteAAltParam;
-		const static Info::Elem CrossFadeTimeAlt      = CrossfadeAAltParam;
+	struct MappingA {
+		const static Info::Elem TimeKnob = TimeAKnob;
+		const static Info::Elem FeedbackKnob = FeedbackAKnob;
+		const static Info::Elem DelayFeedKnob = DelayFeedAKnob;
+		const static Info::Elem MixKnob = MixAKnob;
+		const static Info::Elem ReverseButton = ReverseAButton;
+		const static Info::Elem HoldButton = HoldAButton;
+		const static Info::Elem ReturnInput = ReturnAIn;
+		const static Info::Elem SendOutput = SendAOut;
+		const static Info::Elem ReverseInput = ReverseAJackIn;
+		const static Info::Elem HoldInput = HoldAJackIn;
+		const static Info::Elem TimeInput = TimeAJackIn;
+		const static Info::Elem FeedbackInput = FeedbackAJackIn;
+		const static Info::Elem DelayFeedInput = DelayFeedAJackIn;
+		const static Info::Elem LoopOutput = LoopAOut;
+		const static Info::Elem TimeLight = TimeALedLight;
+		const static Info::Elem TimeModSwitch = TimeMultASwitch;
+		const static Info::Elem SoftClipEnabledAlt = SoftClipAAltParam;
+		const static Info::Elem AutoMuteEnabledAlt = AutomuteAAltParam;
+		const static Info::Elem CrossFadeTimeAlt = CrossfadeAAltParam;
 		const static Info::Elem DelayFeedTaperModeAlt = DelayfeedATaperAltParam;
-		const static Info::Elem HoldInputModeAlt      = HoldAAltParam;
-		const static Info::Elem ReverseInputModeAlt   = Rev_AAltParam;
-		const static Info::Elem TimeUnquantizedAlt    = TimeAAutoNUnqAltParam;
-		const static Info::Elem StereoModeAlt         = StereoModeAAltParam;
-		const static Info::Elem PingMethodAlt         = PingMethodAltParam;
+		const static Info::Elem HoldInputModeAlt = HoldAAltParam;
+		const static Info::Elem ReverseInputModeAlt = Rev_AAltParam;
+		const static Info::Elem TimeUnquantizedAlt = TimeAAutoNUnqAltParam;
+		const static Info::Elem StereoModeAlt = StereoModeAAltParam;
+		const static Info::Elem PingMethodAlt = PingMethodAltParam;
 	};
 
-	struct MappingB
-	{
-		const static Info::Elem TimeKnob              = TimeBKnob;
-		const static Info::Elem FeedbackKnob          = FeedbackBKnob;
-		const static Info::Elem DelayFeedKnob         = DelayFeedBKnob;
-		const static Info::Elem MixKnob               = MixBKnob;
-		const static Info::Elem ReverseButton         = ReverseBButton;
-		const static Info::Elem HoldButton            = HoldBButton;
+	struct MappingB {
+		const static Info::Elem TimeKnob = TimeBKnob;
+		const static Info::Elem FeedbackKnob = FeedbackBKnob;
+		const static Info::Elem DelayFeedKnob = DelayFeedBKnob;
+		const static Info::Elem MixKnob = MixBKnob;
+		const static Info::Elem ReverseButton = ReverseBButton;
+		const static Info::Elem HoldButton = HoldBButton;
 
-		const static Info::Elem ReturnInput           = ReturnBIn;
-		const static Info::Elem SendOutput            = SendBOut;
-		const static Info::Elem ReverseInput          = ReverseBJackIn;
-		const static Info::Elem HoldInput             = HoldBJackIn;
-		const static Info::Elem TimeInput             = TimeBJackIn;
-		const static Info::Elem FeedbackInput         = FeedbackBJackIn;
-		const static Info::Elem DelayFeedInput        = DelayFeedBJackIn;
-		const static Info::Elem LoopOutput            = LoopBOut;
-		const static Info::Elem TimeLight             = TimeBLedLight;
-		const static Info::Elem TimeModSwitch         = TimeMultBSwitch;
-		const static Info::Elem SoftClipEnabledAlt    = SoftClipBAltParam;
-		const static Info::Elem AutoMuteEnabledAlt    = AutomuteBAltParam;
-		const static Info::Elem CrossFadeTimeAlt      = CrossfadeBAltParam;
+		const static Info::Elem ReturnInput = ReturnBIn;
+		const static Info::Elem SendOutput = SendBOut;
+		const static Info::Elem ReverseInput = ReverseBJackIn;
+		const static Info::Elem HoldInput = HoldBJackIn;
+		const static Info::Elem TimeInput = TimeBJackIn;
+		const static Info::Elem FeedbackInput = FeedbackBJackIn;
+		const static Info::Elem DelayFeedInput = DelayFeedBJackIn;
+		const static Info::Elem LoopOutput = LoopBOut;
+		const static Info::Elem TimeLight = TimeBLedLight;
+		const static Info::Elem TimeModSwitch = TimeMultBSwitch;
+		const static Info::Elem SoftClipEnabledAlt = SoftClipBAltParam;
+		const static Info::Elem AutoMuteEnabledAlt = AutomuteBAltParam;
+		const static Info::Elem CrossFadeTimeAlt = CrossfadeBAltParam;
 		const static Info::Elem DelayFeedTaperModeAlt = DelayfeedBTaperAltParam;
-		const static Info::Elem HoldInputModeAlt      = HoldBAltParam;
-		const static Info::Elem ReverseInputModeAlt   = Rev_BAltParam;
-		const static Info::Elem TimeUnquantizedAlt    = TimeBAutoNUnqAltParam;
-		const static Info::Elem StereoModeAlt         = StereoModeBAltParam;
-		const static Info::Elem PingMethodAlt         = PingMethodAltParam;
-	};	
+		const static Info::Elem HoldInputModeAlt = HoldBAltParam;
+		const static Info::Elem ReverseInputModeAlt = Rev_BAltParam;
+		const static Info::Elem TimeUnquantizedAlt = TimeBAutoNUnqAltParam;
+		const static Info::Elem StereoModeAlt = StereoModeBAltParam;
+		const static Info::Elem PingMethodAlt = PingMethodAltParam;
+	};
 
 	DLDChannel<DLDCore, MappingA> channelA;
 	DLDChannel<DLDCore, MappingB> channelB;
@@ -107,8 +101,8 @@ private:
 
 public:
 	DLDCore()
-		: channelA(this), channelB(this)
-	{
+		: channelA(this)
+		, channelB(this) {
 	}
 
 	void update() override {
@@ -124,14 +118,15 @@ public:
 
 		// Input normalization
 		channelA.io.audioIn = getInput<InAIn>().value_or(0);
-		channelB.io.audioIn = isPatched<InBIn>() ? getInput<InBIn>().value_or(0) : channelA.io.audioIn; 
+		channelB.io.audioIn = isPatched<InBIn>() ? getInput<InBIn>().value_or(0) : channelA.io.audioIn;
 
 		channelA.update();
 		channelB.update();
 
 		// Output normalization
 		setOutput<OutBOut>(channelB.io.audioOut);
-		setOutput<OutAOut>(isPatched<OutBOut>() ? channelA.io.audioOut : (channelA.io.audioOut + channelB.io.audioOut) / 2);
+		setOutput<OutAOut>(isPatched<OutBOut>() ? channelA.io.audioOut :
+												  (channelA.io.audioOut + channelB.io.audioOut) / 2);
 
 		// only map channel B to common controls
 		setLED<PingButton>(channelB.io.pingOut);
@@ -143,28 +138,24 @@ public:
 		channelB.set_samplerate(sr);
 	}
 
-	struct SaveState_t
-	{
-		uint32_t ping_time;
+	struct SaveState_t {
+		uint32_t ping_time{};
 	};
-	SaveState_t saveState;
+	SaveState_t saveState{};
 
-	void load_state(std::string_view state_data) override 
-	{
+	void load_state(std::string_view state_data) override {
 		auto raw_data = Base64::decode(state_data);
 
 		std::error_code ec;
 		auto newSaveState = alpaca::deserialize<alpaca::options::with_version, SaveState_t>(raw_data, ec);
-		if (!ec)
-		{
+		if (!ec) {
 			// store current state so it can be applied later
 			saveState = newSaveState;
 			applySaveState();
 		}
 	}
 
-	std::string save_state() override 
-	{
+	std::string save_state() override {
 		populateSaveState();
 
 		std::vector<uint8_t> bytes;
@@ -173,14 +164,12 @@ public:
 		return Base64::encode({bytes.data(), bytes.size()});
 	}
 
-	void applySaveState() 
-	{
+	void applySaveState() {
 		channelA.set_ping_time(saveState.ping_time);
 		channelB.set_ping_time(saveState.ping_time);
 	}
 
-	void populateSaveState()
-	{
+	void populateSaveState() {
 		saveState.ping_time = channelA.get_ping_time();
 	}
 
@@ -191,7 +180,7 @@ public:
 	// clang-format on
 
 private:
-	static constexpr float TriggerThresholdInVolt    = 0.1f;
+	static constexpr float TriggerThresholdInVolt = 0.1f;
 	static constexpr float ClockOuputFullScaleInVolt = 5.0f;
 };
 
--- 4ms/core/looping-delay/DLDChannel.hh
+++ 4ms/core/looping-delay/DLDChannel.hh
@@ -1,8 +1,8 @@
 #include "CoreModules/elements/base_element.hh"
-#include "src/flags.hh"
 #include "src/controls.hh"
-#include "src/params.hh"
+#include "src/flags.hh"
 #include "src/looping_delay.hh"
+#include "src/params.hh"
 
 // #define DEBUG_LOGGING
 
@@ -13,95 +13,83 @@
 namespace MetaModule
 {
 
-template <class Parent, class Mapping>
-class DLDChannel
-{
+template<class Parent, class Mapping>
+class DLDChannel {
 private:
 	template<typename Parent::Info::Elem EL>
-	void setOutput(auto val)
-	{
+	void setOutput(auto val) {
 		return parent->template setOutput<EL>(val);
 	}
 
 	template<typename Parent::Info::Elem EL>
-	auto getInput()
-	{
+	auto getInput() {
 		return parent->template getInput<EL>();
 	}
 
 	template<typename Parent::Info::Elem EL, typename VAL>
-	void setLED(const VAL &value)
-	{
+	void setLED(const VAL &value) {
 		return parent->template setLED<EL>(value);
 	}
 
 	template<typename Parent::Info::Elem EL>
-	auto getState()
-	{
+	auto getState() {
 		return parent->template getState<EL>();
 	}
 
 private:
-	Parent* parent;
+	Parent *parent;
 
 public:
-	DLDChannel(Parent* parent_)
+	DLDChannel(Parent *parent_)
 		: parent(parent_)
 		, params(controls, flags)
 		, looping_delay(params, flags)
 		, audioBufferFillCount(0)
 		, uiUpdateCounter(0)
-		, sampleRate(looping_delay.DefaultSampleRate)
-		{
-			for (auto& sample : outBlock)
-			{
-				for (auto& channel : sample.chan)
-				{
-					channel = 0;
-				}
+		, sampleRate(looping_delay.DefaultSampleRate) {
+		for (auto &sample : outBlock) {
+			for (auto &channel : sample.chan) {
+				channel = 0;
 			}
 		}
+	}
 
-    void update()
-    {
+	void update() {
 		params.timer.inc();
 
-		if (uiUpdateCounter >= params.currentSampleRate/uiUpdateRateHz)
-		{
+		if (uiUpdateCounter >= params.currentSampleRate / uiUpdateRateHz) {
 			uiUpdateCounter = 0;
 
 			sideloadDrivers();
 			handleAltParameters();
 		}
 
-		if (audioBufferFillCount >= inBlock.size())
-		{
+		if (audioBufferFillCount >= inBlock.size()) {
 			audioBufferFillCount = 0;
 
 			params.update();
-			looping_delay.update(inBlock, outBlock);		
+			looping_delay.update(inBlock, outBlock);
 		}
 
 		packUnpackBlockBuffers();
 		audioBufferFillCount++;
 		uiUpdateCounter++;
-    }
+	}
 
-    void set_samplerate(float sr) {
+	void set_samplerate(float sr) {
 
 		auto newSampleRate = uint32_t(std::round(sr));
 
-		if (newSampleRate != sampleRate)
-		{
-			#ifdef DEBUG_LOGGING
+		if (newSampleRate != sampleRate) {
+#ifdef DEBUG_LOGGING
 			printf("Switch sample rate %u -> %u\n", sampleRate, newSampleRate);
-			#endif
+#endif
 
 			sampleRate = newSampleRate;
 
 			looping_delay.set_samplerate(sampleRate);
 		}
-    }
+	}
 
 	void set_ping_time(uint32_t ping_time) {
 		looping_delay.set_ping_time(ping_time);
@@ -112,8 +100,7 @@ public:
 	}
 
 public:
-	struct ModuleIO_t
-	{
+	struct ModuleIO_t {
 		float audioIn;
 		float audioOut;
 		bool pingButtonIn;
@@ -124,46 +111,44 @@ public:
 	ModuleIO_t io;
 
 private:
-	void sideloadDrivers()
-	{
-		auto ConvertKnobFunc = [](float val) -> int16_t
-		{
+	void sideloadDrivers() {
+		auto ConvertKnobFunc = [](float val) -> int16_t {
 			// 0..1 -> 0..4095
 			return std::clamp<int16_t>(val * 4096.f, 0, 4095);
 		};
 
-		auto ConvertCVFunc = [](float val) -> int16_t
-		{
+		auto ConvertCVFunc = [](float val) -> int16_t {
 			// 0..+5V -> 0..4095
 			return std::clamp<int16_t>(val / 5.f * 4096.f, 0, 4095);
 		};
 
-		auto ConvertTimeCVFunc = [](float val) -> int16_t
-		{
+		auto ConvertTimeCVFunc = [](float val) -> int16_t {
 			// -5V..+5V -> 4095..0
-			return std::clamp<int16_t>((5.f - val)/10.f * 4096.f, 0, 4095);
+			return std::clamp<int16_t>((5.f - val) / 10.f * 4096.f, 0, 4095);
 		};
 
-		auto Convert3WaySwitchStateFunc = [](auto inState) -> LDKit::Mocks::SwitchPos
-		{
-			switch (inState)
-			{
-				case Toggle3pos::State_t::DOWN: return LDKit::Mocks::SwitchPos::Down;
-				case Toggle3pos::State_t::UP: return LDKit::Mocks::SwitchPos::Up;
-				case Toggle3pos::State_t::CENTER: return LDKit::Mocks::SwitchPos::Center;
-				default: return LDKit::Mocks::SwitchPos::Invalid;
+		auto Convert3WaySwitchStateFunc = [](auto inState) -> LDKit::Mocks::SwitchPos {
+			switch (inState) {
+				case Toggle3pos::State_t::DOWN:
+					return LDKit::Mocks::SwitchPos::Down;
+				case Toggle3pos::State_t::UP:
+					return LDKit::Mocks::SwitchPos::Up;
+				case Toggle3pos::State_t::CENTER:
+					return LDKit::Mocks::SwitchPos::Center;
+				default:
+					return LDKit::Mocks::SwitchPos::Invalid;
 			}
 		};
 
 		// Knobs
-		controls.sideload_pot(LDKit::TimePot,      ConvertKnobFunc(getState<Mapping::TimeKnob>()));
-		controls.sideload_pot(LDKit::FeedbackPot,  ConvertKnobFunc(getState<Mapping::FeedbackKnob>()));
+		controls.sideload_pot(LDKit::TimePot, ConvertKnobFunc(getState<Mapping::TimeKnob>()));
+		controls.sideload_pot(LDKit::FeedbackPot, ConvertKnobFunc(getState<Mapping::FeedbackKnob>()));
 		controls.sideload_pot(LDKit::DelayFeedPot, ConvertKnobFunc(getState<Mapping::DelayFeedKnob>()));
-		controls.sideload_pot(LDKit::MixPot,       ConvertKnobFunc(getState<Mapping::MixKnob>()));
+		controls.sideload_pot(LDKit::MixPot, ConvertKnobFunc(getState<Mapping::MixKnob>()));
 
 		// CVs
-		controls.sideload_cv(LDKit::TimeCV,      ConvertTimeCVFunc(getInput<Mapping::TimeInput>().value_or(0)));
-		controls.sideload_cv(LDKit::FeedbackCV,  ConvertCVFunc(getInput<Mapping::FeedbackInput>().value_or(0)));
+		controls.sideload_cv(LDKit::TimeCV, ConvertTimeCVFunc(getInput<Mapping::TimeInput>().value_or(0)));
+		controls.sideload_cv(LDKit::FeedbackCV, ConvertCVFunc(getInput<Mapping::FeedbackInput>().value_or(0)));
 		controls.sideload_cv(LDKit::DelayFeedCV, ConvertCVFunc(getInput<Mapping::DelayFeedInput>().value_or(0)));
 		// controls.sideload_cv(LDKit::MixCV,       ConvertCVFunc(getInput<Mapping::MixInput>().value_or(0)));
 
@@ -171,7 +156,7 @@ private:
 		setLED<Mapping::ReverseButton>(controls.rev_led.sideload_get());
 		setLED<Mapping::HoldButton>(controls.inf_led.sideload_get());
 
-		setLED<Mapping::TimeLight>(params.timer.loop_led.sideload_get() ? 1.0f: 0.0f);
+		setLED<Mapping::TimeLight>(params.timer.loop_led.sideload_get() ? 1.0f : 0.0f);
 
 		// Buttons
 		controls.rev_button.register_state(getState<Mapping::ReverseButton>() == MomentaryButton::State_t::PRESSED);
@@ -194,72 +179,70 @@ private:
 		controls.ping_button.register_state(io.pingButtonIn);
 	}
 
-	void handleAltParameters()
-	{
-		params.settings.soft_clip                = getState<Mapping::SoftClipEnabledAlt>() == 0;
-		params.settings.auto_mute                = getState<Mapping::AutoMuteEnabledAlt>() == 0;
+	void handleAltParameters() {
+		params.settings.soft_clip = getState<Mapping::SoftClipEnabledAlt>() == 0;
+		params.settings.auto_mute = getState<Mapping::AutoMuteEnabledAlt>() == 0;
 		params.settings.auto_unquantize_timejack = getState<Mapping::TimeUnquantizedAlt>() == 1;
-		params.settings.stereo_mode              = getState<Mapping::StereoModeAlt>() == 1;
-		params.settings.log_delay_feed           = getState<Mapping::DelayFeedTaperModeAlt>() == 0;
-		params.settings.rev_jack                 = getState<Mapping::ReverseInputModeAlt>() == 0 ? LDKit::GateType::Trig : LDKit::GateType::Gate;
-		params.settings.inf_jack                 = getState<Mapping::HoldInputModeAlt>()    == 0 ? LDKit::GateType::Trig : LDKit::GateType::Gate;
+		params.settings.stereo_mode = getState<Mapping::StereoModeAlt>() == 1;
+		params.settings.log_delay_feed = getState<Mapping::DelayFeedTaperModeAlt>() == 0;
+		params.settings.rev_jack =
+			getState<Mapping::ReverseInputModeAlt>() == 0 ? LDKit::GateType::Trig : LDKit::GateType::Gate;
+		params.settings.inf_jack =
+			getState<Mapping::HoldInputModeAlt>() == 0 ? LDKit::GateType::Trig : LDKit::GateType::Gate;
 
-		static constexpr std::array<uint32_t, 7> CrossfadeSamples      = {1, 96, 192, 384, 1200, 4800, 12000};
-		static constexpr std::array<uint32_t, 7> WriteCrossfadeSamples = {1, 96, 192, 192, 192,  4800,  4800};
+		static constexpr std::array<uint32_t, 7> CrossfadeSamples = {1, 96, 192, 384, 1200, 4800, 12000};
+		static constexpr std::array<uint32_t, 7> WriteCrossfadeSamples = {1, 96, 192, 192, 192, 4800, 4800};
 
-		auto AdaptLengthToSampleRateFunc = [this](auto lengthInSamples)
-		{
+		auto AdaptLengthToSampleRateFunc = [this](auto lengthInSamples) {
 			// make sure this never becomes zero
 			return std::max<uint32_t>(1, uint32_t(lengthInSamples * sampleRate / looping_delay.DefaultSampleRate));
 		};
 
-		params.settings.crossfade_samples       = AdaptLengthToSampleRateFunc(CrossfadeSamples[getState<Mapping::CrossFadeTimeAlt>()]);
-		params.settings.crossfade_rate          = params.settings.calc_fade_increment(params.settings.crossfade_samples);
-		params.settings.write_crossfade_samples = AdaptLengthToSampleRateFunc(WriteCrossfadeSamples[getState<Mapping::CrossFadeTimeAlt>()]);
-		params.settings.write_crossfade_rate    = params.settings.calc_fade_increment(params.settings.write_crossfade_samples);
+		params.settings.crossfade_samples =
+			AdaptLengthToSampleRateFunc(CrossfadeSamples[getState<Mapping::CrossFadeTimeAlt>()]);
+		params.settings.crossfade_rate = params.settings.calc_fade_increment(params.settings.crossfade_samples);
+		params.settings.write_crossfade_samples =
+			AdaptLengthToSampleRateFunc(WriteCrossfadeSamples[getState<Mapping::CrossFadeTimeAlt>()]);
+		params.settings.write_crossfade_rate =
+			params.settings.calc_fade_increment(params.settings.write_crossfade_samples);
 
-		static constexpr std::array<PingMethod,5> PingMethods = {
-			PingMethod::LINEAR_AVERAGE_4,
-			PingMethod::MOVING_AVERAGE_2,
-			PingMethod::ONE_TO_ONE,
-			PingMethod::IGNORE_FLAT_DEVIATION_10,
-			PingMethod::IGNORE_PERCENT_DEVIATION
-		};
+		static constexpr std::array<PingMethod, 5> PingMethods = {PingMethod::LINEAR_AVERAGE_4,
+																  PingMethod::MOVING_AVERAGE_2,
+																  PingMethod::ONE_TO_ONE,
+																  PingMethod::IGNORE_FLAT_DEVIATION_10,
+																  PingMethod::IGNORE_PERCENT_DEVIATION};
 
 		params.settings.ping_method = PingMethods[getState<Mapping::PingMethodAlt>()];
 	}
 
 private:
-	void packUnpackBlockBuffers()
-	{
-		constexpr float AudioFullScale = ((1<<(AudioStreamConf::SampleBits-1)) -1);
+	void packUnpackBlockBuffers() {
+		constexpr float AudioFullScale = ((1 << (AudioStreamConf::SampleBits - 1)) - 1);
 
-		auto clamp = [](auto val, auto max_amplitude)
-		{
+		auto clamp = [](auto val, auto max_amplitude) {
 			return std::min(std::max(val, -max_amplitude), max_amplitude);
 		};
 
-		auto InputConversionFunc = [clamp](auto inputInV) -> int32_t
-		{
+		auto InputConversionFunc = [clamp](auto inputInV) -> int32_t {
 			float normalized = clamp(inputInV / AudioInputFullScaleInVolt, 1.0f);
 			return int32_t(normalized * AudioFullScale);
 		};
 
-		auto OutputConversionFunc = [](auto output) -> float
-		{
+		auto OutputConversionFunc = [](auto output) -> float {
 			return float(output) / AudioFullScale * AudioOutputFullScaleInVolt;
 		};
-		
-		inBlock[audioBufferFillCount] = {InputConversionFunc(getInput<Mapping::ReturnInput>().value_or(0)), InputConversionFunc(io.audioIn)};
+
+		inBlock[audioBufferFillCount] = {InputConversionFunc(getInput<Mapping::ReturnInput>().value_or(0)),
+										 InputConversionFunc(io.audioIn)};
 
 		io.audioOut = OutputConversionFunc(outBlock[audioBufferFillCount].chan[1]);
 		setOutput<Mapping::SendOutput>(OutputConversionFunc(outBlock[audioBufferFillCount].chan[0]));
 	}
 
 private:
-	static constexpr float AudioInputFullScaleInVolt    = 10.0f;
-	static constexpr float AudioOutputFullScaleInVolt   = 10.0f;
-	static constexpr float TriggerThresholdInVolt       = 0.1f;
+	static constexpr float AudioInputFullScaleInVolt = 10.0f;
+	static constexpr float AudioOutputFullScaleInVolt = 10.0f;
+	static constexpr float TriggerThresholdInVolt = 0.1f;
 	static constexpr float TriggerOutputFullScaleInVolt = 5.0f;
 	static constexpr std::size_t uiUpdateRateHz = 3000;
 
@@ -277,6 +260,6 @@ private:
 
 private:
 	uint32_t sampleRate;
-}; 
+};
 
-}
+} // namespace MetaModule
