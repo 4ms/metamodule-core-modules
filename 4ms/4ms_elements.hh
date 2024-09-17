b5c76006c38cf0edcd52e9c81b6a3fc89439fab0
Dan Green <danngreen1@gmail.com>
2024-09-16 21:53:04 -0700

    Rename DefaultValue => default_value

--- 4ms/4ms_elements.hh
+++ 4ms/4ms_elements.hh
@@ -13,7 +13,7 @@ struct Knob9mm : Knob {
 	constexpr Knob9mm() = default;
 	constexpr Knob9mm(BaseElement b, float defaultValue = 0.5f)
 		: Knob{{{b, "4ms/comp/knob9mm_x.png"}}} {
-		DefaultValue = defaultValue;
+		default_value = defaultValue;
 	}
 };
 
@@ -21,7 +21,7 @@ struct DaviesLargeKnob : Knob {
 	constexpr DaviesLargeKnob() = default;
 	constexpr DaviesLargeKnob(BaseElement b, float defaultValue = 0.5f)
 		: Knob{{{b, "4ms/comp/knob_large_x.png"}}} {
-		DefaultValue = defaultValue;
+		default_value = defaultValue;
 	}
 };
 
@@ -29,7 +29,7 @@ struct Davies1900hBlackKnob : Knob {
 	constexpr Davies1900hBlackKnob() = default;
 	constexpr Davies1900hBlackKnob(BaseElement b, float defaultValue = 0.5f)
 		: Knob{{{b, "4ms/comp/knob_x.png"}}} {
-		DefaultValue = defaultValue;
+		default_value = defaultValue;
 	}
 };
 
@@ -40,7 +40,7 @@ struct Slider25mmHorizLED : SliderLight {
 	constexpr Slider25mmHorizLED() = default;
 	constexpr Slider25mmHorizLED(BaseElement b, float defaultValue = 0.5f)
 		: SliderLight{{{{b, "4ms/comp/slider_horiz_x.png"}}, "4ms/comp/slider_horiz_handle_x.png"}, Colors565::White} {
-		DefaultValue = defaultValue;
+		default_value = defaultValue;
 	}
 };
 
@@ -48,7 +48,7 @@ struct Slider25mmVertLED : SliderLight {
 	constexpr Slider25mmVertLED() = default;
 	constexpr Slider25mmVertLED(BaseElement b, float defaultValue = 0.5f)
 		: SliderLight{{{{b, "4ms/comp/slider_x.png"}}, "4ms/comp/slider_handle_x.png"}, Colors565::White} {
-		DefaultValue = defaultValue;
+		default_value = defaultValue;
 	}
 };
 
