bb1733d52273018bd38581eee27788cab813698b
Dan Green <danngreen1@gmail.com>
2024-10-08 12:49:53 -0700

    AudioExpander input jacks names

--- hub/audio_expander_defs.hh
+++ hub/audio_expander_defs.hh
@@ -1,16 +1,20 @@
+#include "panel_medium_defs.hh"
+#include <algorithm>
 #include <array>
 #include <cstdint>
 #include <string_view>
 
 namespace MetaModule::AudioExpander
 {
-static constexpr uint32_t NumInJacks = 8;
+/// Ins
+
+static constexpr uint32_t NumInJacks = 6;
 static constexpr uint32_t NumUserFacingInJacks = NumInJacks;
 
-static constexpr std::array<uint32_t, 6> in_order{0, 1, 2, 3, 4, 5};
+static constexpr std::array<uint32_t, NumUserFacingInJacks> in_order{0, 1, 2, 3, 4, 5};
 
 static constexpr std::array<std::string_view, NumUserFacingInJacks> InJackNames{
-	"Out9", "Out10", "Out11", "Out12", "Out13", "Out14"};
+	"In9", "In10", "In11", "In12", "In13", "In14"};
 
 static constexpr std::array<std::string_view, NumUserFacingInJacks> InJackAbbrevs{"9", "10", "11", "12", "13", "14"};
 
@@ -19,6 +23,13 @@ static constexpr std::string_view get_map_injack_name(uint32_t id) {
 		return InJackNames[id];
 	return "?";
 }
+
+static constexpr uint32_t get_map_injack_num(uint32_t id) {
+	id = std::clamp(id, 0u, NumUserFacingInJacks);
+	return id + PanelDef::NumUserFacingInJacks;
+}
+
+//// Outs
 
 static constexpr std::array<uint32_t, 8> out_order{0, 4, 6, 7, 5, 3, 1, 2};
 
@@ -37,6 +48,12 @@ static constexpr std::string_view get_map_outjack_name(uint32_t id) {
 	return "?";
 }
 
+static constexpr uint32_t get_map_outjack_num(uint32_t id) {
+	id = std::clamp(id, 0u, NumUserFacingOutJacks);
+	return id + PanelDef::NumUserFacingOutJacks;
+}
+
+// Jack sensing
 constexpr inline unsigned jacksense_pin_order[14] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
 constexpr inline bool jack_is_patched(uint32_t jack_sense_reading, unsigned panel_jack_idx) {
