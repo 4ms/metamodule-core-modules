de66f931ad40a9c8637649c7a52c3f0252858e6e
Dan Green <danngreen1@gmail.com>
2024-10-08 08:54:30 -0700

    Swap Ins<->Outs on AudioExpander SVG

--- hub/MMAIO_info.hh
+++ hub/MMAIO_info.hh
@@ -6,47 +6,47 @@
 
 namespace MetaModule
 {
-struct MMAIOInfo : ModuleInfoBase {
-    static constexpr std::string_view slug{"MMAIO"};
-    static constexpr std::string_view description{"MetaModule Audio Expander"};
+struct MMAudioExpanderInfo : ModuleInfoBase {
+    static constexpr std::string_view slug{"MMAudioExpander"};
+    static constexpr std::string_view description{"MetaModule Audio Expander MM AIO"};
     static constexpr uint32_t width_hp = 6;
-    static constexpr std::string_view svg_filename{"res/modules/MMAIO_artwork.svg"};
-    static constexpr std::string_view png_filename{"4ms/fp/MMAIO.png"};
+    static constexpr std::string_view svg_filename{"res/modules/MMAudioExpander_artwork.svg"};
+    static constexpr std::string_view png_filename{"4ms/fp/MMAudioExpander.png"};
 
     using enum Coords;
 
     static constexpr std::array<Element, 14> Elements{{
-		AnalogJackInput4ms{{to_mm<72>(18.38), to_mm<72>(52.21), Center, "In7", ""}},
-		AnalogJackInput4ms{{to_mm<72>(61.9), to_mm<72>(52.21), Center, "In8", ""}},
-		AnalogJackInput4ms{{to_mm<72>(18.38), to_mm<72>(93.61), Center, "In9", ""}},
-		AnalogJackInput4ms{{to_mm<72>(61.9), to_mm<72>(93.61), Center, "In10", ""}},
-		AnalogJackInput4ms{{to_mm<72>(18.38), to_mm<72>(135.01), Center, "In11", ""}},
-		AnalogJackInput4ms{{to_mm<72>(61.9), to_mm<72>(135.01), Center, "In12", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(18.43), to_mm<72>(187.22), Center, "Out9", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(61.94), to_mm<72>(187.22), Center, "Out10", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(18.43), to_mm<72>(228.62), Center, "Out11", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(61.94), to_mm<72>(228.62), Center, "Out12", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(18.43), to_mm<72>(270.02), Center, "Out13", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(61.94), to_mm<72>(270.02), Center, "Out14", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(18.43), to_mm<72>(311.42), Center, "Out15", ""}},
-		AnalogJackOutput4ms{{to_mm<72>(61.94), to_mm<72>(311.42), Center, "Out16", ""}},
+		AnalogJackOutput4ms{{to_mm<72>(18.38), to_mm<72>(52.21), Center, "In7", ""}},
+		AnalogJackOutput4ms{{to_mm<72>(61.9), to_mm<72>(52.21), Center, "In8", ""}},
+		AnalogJackOutput4ms{{to_mm<72>(18.38), to_mm<72>(93.61), Center, "In9", ""}},
+		AnalogJackOutput4ms{{to_mm<72>(61.9), to_mm<72>(93.61), Center, "In10", ""}},
+		AnalogJackOutput4ms{{to_mm<72>(18.38), to_mm<72>(135.01), Center, "In11", ""}},
+		AnalogJackOutput4ms{{to_mm<72>(61.9), to_mm<72>(135.01), Center, "In12", ""}},
+		AnalogJackInput4ms{{to_mm<72>(18.43), to_mm<72>(187.22), Center, "Out9", ""}},
+		AnalogJackInput4ms{{to_mm<72>(61.94), to_mm<72>(187.22), Center, "Out10", ""}},
+		AnalogJackInput4ms{{to_mm<72>(18.43), to_mm<72>(228.62), Center, "Out11", ""}},
+		AnalogJackInput4ms{{to_mm<72>(61.94), to_mm<72>(228.62), Center, "Out12", ""}},
+		AnalogJackInput4ms{{to_mm<72>(18.43), to_mm<72>(270.02), Center, "Out13", ""}},
+		AnalogJackInput4ms{{to_mm<72>(61.94), to_mm<72>(270.02), Center, "Out14", ""}},
+		AnalogJackInput4ms{{to_mm<72>(18.43), to_mm<72>(311.42), Center, "Out15", ""}},
+		AnalogJackInput4ms{{to_mm<72>(61.94), to_mm<72>(311.42), Center, "Out16", ""}},
 }};
 
     enum class Elem {
-        In7In,
-        In8In,
-        In9In,
-        In10In,
-        In11In,
-        In12In,
-        Out9Out,
-        Out10Out,
-        Out11Out,
-        Out12Out,
-        Out13Out,
-        Out14Out,
-        Out15Out,
-        Out16Out,
+        In7Out,
+        In8Out,
+        In9Out,
+        In10Out,
+        In11Out,
+        In12Out,
+        Out9In,
+        Out10In,
+        Out11In,
+        Out12In,
+        Out13In,
+        Out14In,
+        Out15In,
+        Out16In,
     };
 
     // Legacy naming (safe to remove once all legacy 4ms CoreModules are converted)
@@ -54,24 +54,24 @@ struct MMAIOInfo : ModuleInfoBase {
     
     
     enum {
-        InputIn7, 
-        InputIn8, 
-        InputIn9, 
-        InputIn10, 
-        InputIn11, 
-        InputIn12, 
+        InputOut9, 
+        InputOut10, 
+        InputOut11, 
+        InputOut12, 
+        InputOut13, 
+        InputOut14, 
+        InputOut15, 
+        InputOut16, 
         NumInJacks,
     };
     
     enum {
-        OutputOut9, 
-        OutputOut10, 
-        OutputOut11, 
-        OutputOut12, 
-        OutputOut13, 
-        OutputOut14, 
-        OutputOut15, 
-        OutputOut16, 
+        OutputIn7, 
+        OutputIn8, 
+        OutputIn9, 
+        OutputIn10, 
+        OutputIn11, 
+        OutputIn12, 
         NumOutJacks,
     };
     
