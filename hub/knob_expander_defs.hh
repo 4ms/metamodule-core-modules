f47df9eae798d167e60424549bbae7d2350ac3af
Dan Green <danngreen1@gmail.com>
2024-10-12 19:28:25 -0700

    Add KnobExpander defs

---
+++ hub/knob_expander_defs.hh
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdint>
+
+namespace MetaModule::KnobExpander
+{
+
+inline constexpr uint32_t NumKnobsPerExpander = 8;
+inline constexpr uint32_t MaxExpanders = 4;
+
+} // namespace MetaModule::KnobExpander
