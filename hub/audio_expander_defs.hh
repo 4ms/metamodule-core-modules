38f67dd064c5622a376e2ffa83fedb71d17e8a70
Dan Green <danngreen1@gmail.com>
2024-10-22 16:48:52 -0700

    Fix audio expander jack index

--- hub/audio_expander_defs.hh
+++ hub/audio_expander_defs.hh
@@ -25,8 +25,9 @@ static constexpr std::string_view get_map_injack_name(uint32_t id) {
 	return "?";
 }
 
+// 0..5 => 8..13
 static constexpr uint32_t get_map_injack_num(uint32_t id) {
-	id = std::clamp<uint32_t>(id, 0u, NumUserFacingInJacks);
+	id = std::clamp<uint32_t>(id, 0u, NumInJacks - 1);
 	return id + PanelDef::NumUserFacingInJacks;
 }
 
