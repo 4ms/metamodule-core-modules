498f13cb8ca13785ec06a745090db1bb033021d6
Dan Green <danngreen1@gmail.com>
2025-02-25 17:03:38 -0800

    WIP testing on CLKD with ThorVG

--- 4ms/core/CLKDCore.cc
+++ 4ms/core/CLKDCore.cc
@@ -1,7 +1,9 @@
+#include "../../../../src/medium/debug_raw.h"
 #include "CoreModules/CoreProcessor.hh"
 #include "CoreModules/moduleFactory.hh"
 #include "info/CLKD_info.hh"
 #include "processors/tools/clockPhase.h"
+#include "thorvg/thorvg/inc/thorvg.h"
 #include "util/math.hh"
 
 using namespace MathTools;
@@ -14,14 +16,66 @@ class CLKDCore : public CoreProcessor {
 	using ThisCore = CLKDCore;
 
 public:
-	CLKDCore() = default;
+	CLKDCore() {
+	}
+
+	// tvg::SwCanvas canvas;
+
+	std::unique_ptr<tvg::SwCanvas> canvas = tvg::SwCanvas::gen();
+	// tvg::Shape *circle = tvg::Shape::gen();
+	// tvg::Shape *circle2 = tvg::Shape::gen();
+
+	~CLKDCore() {
+	}
 
 	bool get_canvas_pixels(int display_id, Pixel *pix, uint16_t width, uint16_t height) override {
-		for (uint16_t x = 0; x < width; x++) {
-			for (uint16_t y = 0; y < height; y++) {
-				pix[x + y * width] = Pixel{0x80, 0xA0, 0xC0, 0xF0};
-			}
-		}
+		// auto canvas = MetaModule::make_thorvg_canvas(pix, width, height);
+
+		static bool first = true;
+		// if (first) {
+		// 	first = false;
+		canvas->target(reinterpret_cast<uint32_t *>(pix), width, width, height, tvg::SwCanvas::Colorspace::ARGB8888);
+		// }
+		// canvas->clear();
+
+		//100us
+		auto circle = tvg::Shape::gen();
+		circle->appendCircle(15, 25, 15, 25);
+		circle->fill(0xFF, 0x80, 0x00, 0xCC);
+		canvas->push(std::move(circle));
+
+		//100us
+		auto circle2 = tvg::Shape::gen();
+		circle2->appendCircle(5, 45, 10, clockDivideOffset * 25);
+		circle2->fill(0x00, 0x80, 0xFF, 0x30);
+		canvas->push(std::move(circle2));
+
+		// canvas->draw(true);
+		// canvas->sync();
+
+		// circle2->moveTo(clockDivideOffset * 20, 45);
+		// 65ms
+		canvas->draw();
+
+		//<200ns
+		canvas->sync();
+
+		// delete circle;
+		// delete circle2;
+		// delete canvas;
+
+		// for (uint16_t y = 0; y < height; y++) {
+		// 	for (uint16_t x = 0; x < width; x++) {
+		// 		if (y < height / 2) {
+		// 			if (x < width / 2)
+		// 				pix[x + y * width] = Pixel{0xFF, 0x00, 0x00, 0xFF};
+		// 			else
+		// 				pix[x + y * width] = Pixel{0x80, 0xA0, 0xC0, 0x8F};
+		// 		} else {
+		// 			pix[x + y * width] = Pixel{0x44, (x < width / 2) ? uint8_t(0x99) : uint8_t(0xFF), 0x00, 0x7F};
+		// 		}
+		// 	}
+		// }
 		return true;
 	}
 
