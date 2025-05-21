e1a7a8bf6c2f3fc9aa2a94dce87406fa19700c7d
Dan Green <danngreen1@gmail.com>
2025-05-21 14:55:35 -0700

    Use AltParamAction, TSP restarts when play is hit again

--- 4ms/info/TSP_info.hh
+++ 4ms/info/TSP_info.hh
@@ -1,21 +1,21 @@
 #pragma once
-#include "CoreModules/4ms/4ms_elements.hh"
 #include "CoreModules/4ms/4ms_element_state_conversions.hh"
+#include "CoreModules/4ms/4ms_elements.hh"
 #include "CoreModules/elements/element_info.hh"
 #include <array>
 
 namespace MetaModule
 {
 struct TSPInfo : ModuleInfoBase {
-    static constexpr std::string_view slug{"TSP"};
-    static constexpr std::string_view description{"Sample Player"};
-    static constexpr uint32_t width_hp = 6;
-    static constexpr std::string_view svg_filename{"res/modules/TSP_artwork.svg"};
-    static constexpr std::string_view png_filename{"4ms/fp/TSP.png"};
+	static constexpr std::string_view slug{"TSP"};
+	static constexpr std::string_view description{"Sample Player"};
+	static constexpr uint32_t width_hp = 6;
+	static constexpr std::string_view svg_filename{"res/modules/TSP_artwork.svg"};
+	static constexpr std::string_view png_filename{"4ms/fp/TSP.png"};
 
-    using enum Coords;
+	using enum Coords;
 
-    static constexpr std::array<Element, 10> Elements{{
+	static constexpr std::array<Element, 10> Elements{{
 		MomentaryRGB7mm{{to_mm<72>(26.196), to_mm<72>(154.699), Center, "Play", ""}},
 		MomentaryRGB7mm{{to_mm<72>(62.339), to_mm<72>(154.65), Center, "Loop", ""}},
 		TSPDisplay{{to_mm<72>(43.203), to_mm<72>(80.133), Center, "Screen", ""}},
@@ -25,52 +25,51 @@ struct TSPInfo : ModuleInfoBase {
 		AnalogJackOutput4ms{{to_mm<72>(61.274), to_mm<72>(291.569), Center, "Right Out", ""}},
 		AnalogJackOutput4ms{{to_mm<72>(43.203), to_mm<72>(250.377), Center, "End Out", ""}},
 		RedLight{{to_mm<72>(43.203), to_mm<72>(140.68), Center, "Busy Light", ""}},
-		AltParamChoiceLabeled{{{to_mm<72>(42.136), to_mm<72>(356.476), Center, "LoadSample", ""}, 1, 0}, {"LoadSample"}},
-}};
+		AltParamAction{to_mm<72>(42.136), to_mm<72>(356.476), Center, "Load Sample...", "Load Sample..."},
+	}};
 
-    enum class Elem {
-        PlayButton,
-        LoopButton,
-        ScreenOut,
-        PlayTrigIn,
-        LoopGateIn,
-        LeftOut,
-        RightOut,
-        EndOut,
-        BusyLight,
-        LoadsampleAltParam,
-    };
+	enum class Elem {
+		PlayButton,
+		LoopButton,
+		ScreenOut,
+		PlayTrigIn,
+		LoopGateIn,
+		LeftOut,
+		RightOut,
+		EndOut,
+		BusyLight,
+		LoadsampleAltParam,
+	};
 
-    // Legacy naming (safe to remove once all legacy 4ms CoreModules are converted)
-    
-    
-    enum {
-        SwitchPlay, 
-        SwitchLoop, 
-        NumSwitches,
-    };
-    
-    enum {
-        InputPlay_Trig, 
-        InputLoop_Gate, 
-        NumInJacks,
-    };
-    
-    enum {
-        OutputScreen, 
-        OutputLeft_Out, 
-        OutputRight_Out, 
-        OutputEnd_Out, 
-        NumOutJacks,
-    };
-    
-    enum {
-        LedBusy_Light, 
-        NumDiscreteLeds,
-    };
-    
-    enum {
-        AltParamLoadsample, 
-    };
+	// Legacy naming (safe to remove once all legacy 4ms CoreModules are converted)
+
+	enum {
+		SwitchPlay,
+		SwitchLoop,
+		NumSwitches,
+	};
+
+	enum {
+		InputPlay_Trig,
+		InputLoop_Gate,
+		NumInJacks,
+	};
+
+	enum {
+		OutputScreen,
+		OutputLeft_Out,
+		OutputRight_Out,
+		OutputEnd_Out,
+		NumOutJacks,
+	};
+
+	enum {
+		LedBusy_Light,
+		NumDiscreteLeds,
+	};
+
+	enum {
+		AltParamLoadsample,
+	};
 };
 } // namespace MetaModule
