3d1b7c8c3e341b068ffab08230e8ca52db21b1e5
Dan Green <danngreen1@gmail.com>
2025-05-24 21:50:05 -0700

    Refactor resampler

--- 4ms/core/tsp/wav_file_stream.hh
+++ 4ms/core/tsp/wav_file_stream.hh
@@ -9,15 +9,13 @@
 namespace MetaModule
 {
 
-template<uint64_t MaxSamples = 1024 * 1024>
+template<uint64_t MaxSamples = 1024 * 1024, uint32_t MaxResamplingRatio = 4>
 struct WavFileStream {
 
 	bool load(std::string_view sample_path) {
 		unload();
 
 		eof = false;
-
-		// TODO: if we already have this file loaded, then just reset the read position
 
 		loaded = drwav_init_file(&wav, sample_path.data(), nullptr);
 		return loaded;
@@ -37,7 +35,6 @@ struct WavFileStream {
 	}
 
 	void read_frames_from_file() {
-		// Read in 4kB chunks:
 		unsigned frames_to_read = ReadBlockBytes / wav.fmt.blockAlign;
 		read_frames_from_file(frames_to_read);
 	}
@@ -50,45 +47,32 @@ struct WavFileStream {
 			// Read blocks of maximum 4kB at a time
 			unsigned frames_to_read = std::min(ReadBlockBytes / wav.fmt.blockAlign, num_frames);
 
-			last_frame_written = wav.readCursorInPCMFrames;
+			next_frame_to_write = wav.readCursorInPCMFrames;
 			auto frames_read = drwav_read_pcm_frames_f32(&wav, frames_to_read, read_buff.data());
 
 			eof = (frames_read != frames_to_read);
 
-			if (frames_read > num_frames) {
+			if (frames_read > frames_to_read) {
 				printf("WavFileStream: Internal error: drwav read more frames than requested\n");
-				frames_read = num_frames;
+				frames_read = frames_to_read;
 			}
 
 			// Push samples into resampler
-			resampler.set_input_stride(wav.channels);
-			resampler.set_output_stride(wav.channels);
 			resampler.set_sample_rate_in_out(wav.sampleRate, out_sr);
 
-			const size_t in_samples = frames_read * wav.channels;
-			const size_t out_samples = in_samples / (wav.sampleRate / out_sr);
-			for (auto chan = 0u; chan < wav.channels; chan++) {
-				auto inp = std::span<const float>{&read_buff[chan], in_samples};
-				auto outp = std::span<float>{&resamp_buff[chan], out_samples};
+			auto input = std::span<const float>(read_buff.data(), frames_read * wav.channels);
+			auto output = resampler.process_block(wav.channels, input);
 
-				resampler.process(chan, inp, outp);
-				// resampler.push(chan, std::span(&read_buff[chan], frames_read));
-			}
-
-			// Pop samples out of resampler, storing in pre_buff
-			for (auto frame = 0u; frame < out_samples / wav.channels; frame++) {
-				if (pre_buff.num_free() >= wav.channels) {
-					for (auto chan = 0u; chan < wav.channels; chan++) {
-						pre_buff.put(resamp_buff[frame * wav.channels + chan]);
-						// pre_buff.put(resampler.pop());
-					}
-				} else {
-					printf("WavFileStream: prebuffer overflow\n");
+			for (auto out : output) {
+				if (!pre_buff.put(out)) {
+					printf("WavFileStream: Buffer overflow\n");
+					// TODO: Handle buffer overflow.
+					// Set drwav read cursor back to this position, pop back if we're not a frame boundary,
+					// set next_frame_to_write, and abort
 				}
-
-				num_frames--;
-				last_frame_written++;
 			}
+			num_frames -= output.size() / wav.channels;
+			next_frame_to_write += output.size() / wav.channels;
 
 			if (eof)
 				break;
@@ -103,7 +87,7 @@ struct WavFileStream {
 	void set_samplerate(float samplerate) {
 		out_sr = samplerate;
 		if (loaded) {
-			resampler.set_sample_rate_in_out(wav.sampleRate, samplerate);
+			resampler.set_sample_rate_in_out(wav.sampleRate, out_sr);
 		}
 	}
 
@@ -124,7 +108,7 @@ struct WavFileStream {
 	}
 
 	uint64_t current_playback_frame() const {
-		return last_frame_written > frames_available() ? last_frame_written - frames_available() : 0;
+		return next_frame_to_write > frames_available() ? next_frame_to_write - frames_available() : 0;
 	}
 
 	uint64_t total_frames() const {
@@ -133,12 +117,12 @@ struct WavFileStream {
 
 	void seek_frame_in_file(uint64_t frame_num = 0) {
 
-		const auto frames_in_prebuff = std::min(MaxSamples / wav.channels, last_frame_written);
+		const auto frames_in_prebuff = std::min(MaxSamples / wav.channels, next_frame_to_write);
 
 		// Optimization:
 		// if we request to seek to a frame that's already in the prebuffer,
 		// just jump the read head to there (no need to read from disk)
-		if (frame_num < last_frame_written && frame_num >= (last_frame_written - frames_in_prebuff)) {
+		if (frame_num < next_frame_to_write && frame_num >= (next_frame_to_write - frames_in_prebuff)) {
 			pre_buff.set_read_pos(frame_num * wav.channels);
 		} else {
 			// Otherwise, we don't have the requested frame in our buffer
@@ -162,7 +146,7 @@ private:
 	void reset_prebuff() {
 		pre_buff.set_write_pos(0);
 		pre_buff.set_read_pos(0);
-		last_frame_written = 0;
+		next_frame_to_write = 0;
 	}
 
 	drwav wav;
@@ -170,7 +154,7 @@ private:
 	bool eof = true;
 	bool loaded = false;
 
-	uint64_t last_frame_written = 0;
+	uint64_t next_frame_to_write = 0;
 
 	LockFreeFifoSpsc<float, MaxSamples> pre_buff;
 
@@ -178,12 +162,13 @@ private:
 	static constexpr unsigned ReadBlockBytes = 4096;
 
 	// read_buff needs to be big enough to hold 4kB of any data converted to floats
-	// e.g. 4kB of 8-bit mono will convert to 4096 floats
+	// Worst case: 4kB of 8-bit mono data will convert to 4096 floats
 	std::array<float, ReadBlockBytes> read_buff;
 
-	// ResamplingRingBuffer<2, 4096> resampler;
-	AudioResampler resampler{2};
-	std::array<float, ReadBlockBytes * 2> resamp_buff;
+	static constexpr auto MaxChannels = 2; //Stereo or Mono files only
+	ResamplingInterleavedBuffer<MaxChannels, ReadBlockBytes, MaxResamplingRatio> resampler;
+	// AudioResampler resampler{2};
+	// std::array<float, ReadBlockBytes * 2> resamp_buff;
 	float out_sr = 48000.f;
 };
 
