7badf46f01f0be3fead14824bcf71b93329e62d1
Dan Green <danngreen1@gmail.com>
2025-06-09 17:10:39 -0700

    Fix broken Drum module:
    - several CV jacks not working
    - envelope times set wrong (bug was setting sustain, not release)
    - Also changed Envelope => Env

--- 4ms/core/DrumCore.cc
+++ 4ms/core/DrumCore.cc
@@ -35,8 +35,14 @@ private:
 	float toneEnvCV = 0;
 	float pitchEnvCV = 0;
 	float FMEnvCV = 0;
+	float ratioCV = 0;
+	float fmAmountCV = 0;
+	float pitchAmountCV = 0;
+	float noiseBlendCV = 0;
 
 	float ratio = 1;
+
+	float tenv;
 
 	InterpArray<float, 4> pitchDecayTimes = {10, 10, 200, 500};
 	InterpArray<float, 4> pitchBreakPoint = {0, 0.1, 0.2, 1};
@@ -46,7 +52,7 @@ private:
 	InterpArray<float, 6> toneHoldTimes = {0, 20, 50, 70, 100, 600};
 	InterpArray<float, 3> toneDecayTimes = {10, 200, 600};
 	InterpArray<float, 4> toneBreakPoint = {0.1, 0.2, 0.8};
-	InterpArray<float, 3> toneReleaseTimes = {10, 500, 4000};
+	InterpArray<float, 3> toneReleaseTimes = {10, 200, 500};
 
 public:
 	DrumCore() {
@@ -61,24 +67,24 @@ public:
 		envelopes[toneEnvelope].set_envelope_time(0, 1);
 		envelopes[toneEnvelope].set_envelope_time(1, 50);
 		envelopes[toneEnvelope].set_envelope_time(2, 100);
-		envelopes[toneEnvelope].set_envelope_time(3, 2000);
+		envelopes[toneEnvelope].set_envelope_time(4, 2000);
 		envelopes[toneEnvelope].set_release_curve(1.0f);
 
 		envelopes[fmEnvelope].set_envelope_time(0, 1);
 		envelopes[fmEnvelope].set_envelope_time(1, 0);
 		envelopes[fmEnvelope].set_envelope_time(2, 300);
-		envelopes[fmEnvelope].set_envelope_time(3, 700);
+		envelopes[fmEnvelope].set_envelope_time(4, 700);
 
 		envelopes[noiseEnvelope].set_envelope_time(0, 1);
 		envelopes[noiseEnvelope].set_envelope_time(1, 0);
 		envelopes[noiseEnvelope].set_envelope_time(2, 30);
-		envelopes[noiseEnvelope].set_envelope_time(3, 700);
+		envelopes[noiseEnvelope].set_envelope_time(4, 700);
 		envelopes[noiseEnvelope].set_release_curve(0.0f);
 
 		envelopes[pitchEnvelope].set_envelope_time(0, 5.0);
 		envelopes[pitchEnvelope].set_envelope_time(1, 0);
 		envelopes[pitchEnvelope].set_envelope_time(2, 50);
-		envelopes[pitchEnvelope].set_envelope_time(3, 2000);
+		envelopes[pitchEnvelope].set_envelope_time(4, 2000);
 
 		setToneEnvelope();
 		setFMEnvelope();
@@ -87,21 +93,26 @@ public:
 	}
 
 	void update() override {
-		auto freqCalc =
-			baseFrequency + (envelopes[pitchEnvelope].update(gateIn) * 4000.0f * (pitchAmount * pitchAmount));
-		osc.set_frequency(1, baseFrequency * ratio);
+		float pitchAmt = std::clamp(pitchAmount + pitchAmountCV, 0.f, 1.f);
+		auto freqCalc = baseFrequency + (envelopes[pitchEnvelope].update(gateIn) * 4000.0f * (pitchAmt * pitchAmt));
+
+		float ratioTot = MathTools::map_value(std::clamp(ratio + ratioCV, 0.f, 1.f), 0.0f, 1.0f, 1.0f, 16.0f);
+		osc.set_frequency(1, baseFrequency * ratioTot);
+
 		if (pitchConnected) {
 			osc.set_frequency(0, freqCalc * MathTools::setPitchMultiple(pitchCV));
 		} else {
 			osc.set_frequency(0, freqCalc);
 		}
 
-		osc.modAmount = envelopes[fmEnvelope].update(gateIn) * fmAmount;
+		osc.modAmount = envelopes[fmEnvelope].update(gateIn) * std::clamp(fmAmount + fmAmountCV, 0.f, 1.f);
 		auto noiseOut = MathTools::randomNumber(-1.0f, 1.0f) * envelopes[noiseEnvelope].update(gateIn);
 
-		auto toneOutput = osc.update() * envelopes[toneEnvelope].update(gateIn);
+		tenv = envelopes[toneEnvelope].update(gateIn);
+		auto toneOutput = osc.update() * tenv;
 
-		drumOutput = MathTools::interpolate(toneOutput, noiseOut, noiseBlend);
+		auto noiseBlendTot = std::clamp(noiseBlend + noiseBlendCV, 0.f, 1.f);
+		drumOutput = MathTools::interpolate(toneOutput, noiseOut, noiseBlendTot);
 	}
 
 	void set_param(int param_id, float val) override {
@@ -109,7 +120,7 @@ public:
 			case Info::KnobPitch:
 				baseFrequency = MathTools::map_value(val, 0.0f, 1.0f, 10.0f, 1000.0f);
 				break;
-			case Info::KnobPitch_Envelope: // pitch envelope
+			case Info::KnobPitch_Env: // pitch envelope
 				basePitchEnvTime = val;
 				setPitchEnvelope();
 				break;
@@ -117,20 +128,20 @@ public:
 				pitchAmount = val;
 				break;
 			case Info::KnobFm_Ratio:
-				ratio = MathTools::map_value(val, 0.0f, 1.0f, 1.0f, 16.0f);
+				ratio = val;
 				break;
-			case Info::KnobFm_Envelope: // fm envelope
+			case Info::KnobFm_Env: // fm envelope
 				baseFMEnvTime = val;
 				setFMEnvelope();
 				break;
 			case Info::KnobFm_Amount:
 				fmAmount = val;
 				break;
-			case Info::KnobTone_Envelope: // tone envelope
+			case Info::KnobTone_Env: // tone envelope
 				baseToneEnvTime = val;
 				setToneEnvelope();
 				break;
-			case Info::KnobNoise_Envelope: // noise envelope
+			case Info::KnobNoise_Env: // noise envelope
 				baseNoiseEnvTime = val;
 				setNoiseEnvelope();
 				break;
@@ -144,19 +155,19 @@ public:
 		switch (param_id) {
 			case Info::KnobPitch:
 				return MathTools::map_value(baseFrequency, 10.0f, 1000.0f, 0.f, 1.f);
-			case Info::KnobPitch_Envelope: // pitch envelope
+			case Info::KnobPitch_Env: // pitch envelope
 				return basePitchEnvTime;
 			case Info::KnobPitch_Amount:
 				return pitchAmount;
 			case Info::KnobFm_Ratio:
 				return MathTools::map_value(ratio, 1.0f, 16.0f, 0.f, 1.f);
-			case Info::KnobFm_Envelope: // fm envelope
+			case Info::KnobFm_Env: // fm envelope
 				return baseFMEnvTime;
 			case Info::KnobFm_Amount:
 				return fmAmount;
-			case Info::KnobTone_Envelope: // tone envelope
+			case Info::KnobTone_Env: // tone envelope
 				return baseToneEnvTime;
-			case Info::KnobNoise_Envelope: // noise envelope
+			case Info::KnobNoise_Env: // noise envelope
 				return baseNoiseEnvTime;
 			case Info::KnobNoise_Blend:
 				return noiseBlend;
@@ -166,33 +177,37 @@ public:
 
 	void setFMEnvelope() {
 		float val = MathTools::constrain(baseFMEnvTime + FMEnvCV, 0.0f, 1.0f);
-		envelopes[fmEnvelope].set_envelope_time(0, MathTools::map_value(val, 0.0f, 1.0f, 1.0f, 100.0f));
-		envelopes[fmEnvelope].set_envelope_time(2, MathTools::map_value(val, 0.0f, 1.0f, 10.0f, 8000.0f));
-		envelopes[fmEnvelope].set_envelope_time(3, MathTools::map_value(val, 0.0f, 1.0f, 10.0f, 3000.0f));
+		envelopes[fmEnvelope].set_envelope_time(Envelope::ATTACK, MathTools::map_value(val, 0.0f, 1.0f, 1.0f, 100.0f));
+		envelopes[fmEnvelope].set_envelope_time(Envelope::DECAY, MathTools::map_value(val, 0.0f, 1.0f, 10.0f, 8000.0f));
+		envelopes[fmEnvelope].set_envelope_time(Envelope::RELEASE,
+												MathTools::map_value(val, 0.0f, 1.0f, 10.0f, 3000.0f));
 		envelopes[fmEnvelope].set_sustain(MathTools::map_value(val, 0.0f, 1.0f, 0.0f, 0.3f));
 	}
 
 	void setToneEnvelope() {
 		float val = MathTools::constrain(baseToneEnvTime + toneEnvCV, 0.0f, 1.0f);
-		envelopes[toneEnvelope].set_envelope_time(0, toneAttackTimes.interp(val));
-		envelopes[toneEnvelope].set_envelope_time(1, toneHoldTimes.interp(val));
-		envelopes[toneEnvelope].set_envelope_time(2, toneDecayTimes.interp(val));
-		envelopes[toneEnvelope].set_envelope_time(3, toneReleaseTimes.interp(val));
+		envelopes[toneEnvelope].set_envelope_time(Envelope::ATTACK, toneAttackTimes.interp(val));
+		envelopes[toneEnvelope].set_envelope_time(Envelope::HOLD, toneHoldTimes.interp(val));
+		envelopes[toneEnvelope].set_envelope_time(Envelope::DECAY, toneDecayTimes.interp(val));
+		envelopes[toneEnvelope].set_envelope_time(Envelope::RELEASE, toneReleaseTimes.interp(val));
 		envelopes[toneEnvelope].set_sustain(toneBreakPoint.interp(val));
 	}
 
 	void setNoiseEnvelope() {
 		float val = MathTools::constrain(baseNoiseEnvTime + noiseEnvCV, 0.0f, 1.0f);
-		envelopes[noiseEnvelope].set_envelope_time(0, MathTools::map_value(val, 0.0f, 1.0f, 1.0f, 50.0f));
-		envelopes[noiseEnvelope].set_envelope_time(2, MathTools::map_value(val, 0.0f, 1.0f, 30.0f, 100.0f));
-		envelopes[noiseEnvelope].set_envelope_time(3, MathTools::map_value(val, 0.0f, 1.0f, 100.0f, 3000.0f));
+		envelopes[noiseEnvelope].set_envelope_time(Envelope::ATTACK,
+												   MathTools::map_value(val, 0.0f, 1.0f, 1.0f, 50.0f));
+		envelopes[noiseEnvelope].set_envelope_time(Envelope::DECAY,
+												   MathTools::map_value(val, 0.0f, 1.0f, 30.0f, 100.0f));
+		envelopes[noiseEnvelope].set_envelope_time(Envelope::RELEASE,
+												   MathTools::map_value(val, 0.0f, 1.0f, 100.0f, 3000.0f));
 		envelopes[noiseEnvelope].set_sustain(MathTools::map_value(val, 0.0f, 1.0f, 0.0f, 0.25f));
 	}
 
 	void setPitchEnvelope() {
 		float val = MathTools::constrain(pitchEnvCV + basePitchEnvTime, 0.0f, 1.0f);
-		envelopes[pitchEnvelope].set_envelope_time(2, pitchDecayTimes.interp(val));
-		envelopes[pitchEnvelope].set_envelope_time(3, pitchReleaseTimes.interp(val));
+		envelopes[pitchEnvelope].set_envelope_time(Envelope::DECAY, pitchDecayTimes.interp(val));
+		envelopes[pitchEnvelope].set_envelope_time(Envelope::RELEASE, pitchReleaseTimes.interp(val));
 		envelopes[pitchEnvelope].set_sustain(pitchBreakPoint.interp(val));
 	}
 
@@ -213,30 +228,33 @@ public:
 			case Info::InputV_Oct_In:
 				pitchCV = val;
 				break;
-			case Info::InputNoise_Envelope_Cv_In:
+			case Info::InputNoise_Env_Cv_In:
 				noiseEnvCV = val;
 				setNoiseEnvelope();
 				break;
-			case Info::InputFm_Envelope_Cv_In:
+			case Info::InputFm_Env_Cv_In:
 				FMEnvCV = val;
 				setFMEnvelope();
 				break;
-			case Info::InputPitch_Envelope_Cv_In:
+			case Info::InputPitch_Env_Cv_In:
 				pitchEnvCV = val;
 				setPitchEnvelope();
 				break;
-			case Info::InputTone_Envelope_Cv_In:
+			case Info::InputTone_Env_Cv_In:
 				toneEnvCV = val;
 				setToneEnvelope();
 				break;
-
 			case Info::InputPitch_Amount_Cv_In:
+				pitchAmountCV = val;
 				break;
 			case Info::InputNoise_Blend_Cv_In:
+				noiseBlendCV = val;
 				break;
 			case Info::InputFm_Amount_Cv_In:
+				fmAmountCV = val;
 				break;
 			case Info::InputRatio_Cv_In:
+				ratioCV = val;
 				break;
 		}
 	}
@@ -244,6 +262,8 @@ public:
 	float get_output(int output_id) const override {
 		if (output_id == Info::OutputAudio_Out)
 			return drumOutput * outputVolts;
+		if (output_id == Info::OutputInverted_Out)
+			return tenv;
 		return 0.f;
 	}
 
