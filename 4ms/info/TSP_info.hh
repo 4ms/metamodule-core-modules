3b88af487ace697cce44a9faa3a185d18550d9fa
Dan Green <danngreen1@gmail.com>
2025-07-09 14:56:10 -0700

    Cleanup with helper element GraphicDisplay

--- 4ms/info/TSP_info.hh
+++ 4ms/info/TSP_info.hh
@@ -24,7 +24,7 @@ struct TSPInfo : ModuleInfoBase {
 		AnalogJackOutput4ms{{to_mm<72>(61.274), to_mm<72>(294.822), Center, "Right Out", ""}},
 		AnalogJackOutput4ms{{to_mm<72>(24.684), to_mm<72>(249.486), Center, "End Out", ""}},
 		AnalogJackOutput4ms{{to_mm<72>(61.274), to_mm<72>(249.486), Center, "Position", ""}},
-		DynamicGraphicDisplay{{to_mm<72>(9.234), to_mm<72>(60.753), TopLeft, "Waveform", "", to_mm<72>(67.938), to_mm<72>(66.398)}},
+		GraphicDisplay{{to_mm<72>(9.234), to_mm<72>(60.753), TopLeft, "Waveform", "", to_mm<72>(67.938), to_mm<72>(66.398)}},
 		TSPDisplay{{to_mm<72>(9.056), to_mm<72>(37.165), TopLeft, "Message", "", to_mm<72>(67.938), to_mm<72>(23.588)}},
 		RedLight{{to_mm<72>(43.203), to_mm<72>(140.68), Center, "Busy Light", ""}},
 		AltParamChoiceLabeled{{{to_mm<72>(66.469), to_mm<72>(48.461), Center, "Play Retrig Mode", ""}, 3, 0}, {"Retrigger", "Stop", "Pause"}},
@@ -49,43 +49,6 @@ struct TSPInfo : ModuleInfoBase {
         WaveformZoomAltParam,
         BufferThresholdAltParam,
         LoadSampleAltParam,
-    };
-
-    // Legacy naming
-    
-    
-    enum {
-        SwitchPlay, 
-        SwitchLoop, 
-        NumSwitches,
-    };
-    
-    enum {
-        InputPlay_Trig, 
-        InputLoop_Gate, 
-        NumInJacks,
-    };
-    
-    enum {
-        OutputLeft_Out, 
-        OutputRight_Out, 
-        OutputEnd_Out, 
-        OutputPosition, 
-        NumOutJacks,
-    };
-    
-    enum {
-        LedWaveform, 
-        LedMessage, 
-        LedBusy_Light, 
-        NumDiscreteLeds,
-    };
-    
-    enum {
-        AltParamLoad_Sample___, 
-        AltParamPlay_Retrig_Mode, 
-        AltParamWaveform_Zoom, 
-        AltParamBuffer_Threshold, 
     };
 
 };
